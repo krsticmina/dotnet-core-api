6c7c23197c5280ca583e098f6118a9f022099e38
Mina Krstic <Mina.Krstic@endava.com>
2022-10-10 13:46:22 +0200

    Added Contracts folder

---
+++ dotnet-core-api/Contracts/V1/ApiRoutesV1.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_core_api.Contracts.V1
+{
+    public static class ApiRoutesV1
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public static readonly string Base = $"{Root}/{Version}/";
+
+    }
+}
---
+++ dotnet-core-api/Contracts/V2/ApiRoutesV2.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_core_api.Contracts.V2
+{
+    public static class ApiRoutesV2
+    {
+        public const string Root = "api";
+        public const string Version = "v2";
+        public static readonly string Base = $"{Root}/{Version}/";
+    }
+}
