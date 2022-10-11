10f6efb51aadcfe71b4763424274d1071d78f28c
Mina Krstic <Mina.Krstic@endava.com>
2022-10-11 14:45:08 +0200

    Created BlogDatabase and added Category entity

---
+++ dotnet-core-api/Data/DbContexts/BlogDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_core_api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_core_api.Data.DbContexts
+{
+    public class BlogDatabaseContext : DbContext
+    {
+        public BlogDatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
